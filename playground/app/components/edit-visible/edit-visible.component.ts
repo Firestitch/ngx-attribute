e4a458a84a18ccbfdd3b000aeef41fa597dc7bbb
Raymond Gigliotti <ray@firestitch.com>
2019-06-05 13:49:41 -0400

    updated fsexample added form validation example

--- playground/app/components/edit-visible/edit-visible.component.ts
+++ playground/app/components/edit-visible/edit-visible.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, SkipSelf } from '@angular/core';
 import { FS_ATTRIBUTE_CONFIG, FS_ATTRIBUTE_FIELD_DATA } from 'src/app/providers';
 import { FsAttributeConfig } from 'src/app/interfaces/attribute-config.interface';
+import { ControlContainer } from '@angular/forms';
 
 
 @Component({
   selector: 'edit-visible',
   templateUrl: 'edit-visible.component.html',
-  styleUrls: ['edit-visible.component.scss']
+  styleUrls: ['edit-visible.component.scss'],
+  // Required when using form validation so that the parent form is visible
+  viewProviders: [{
+    provide: ControlContainer,
+    useFactory: (container: ControlContainer) => container,
+    deps: [[new SkipSelf(), ControlContainer]],
+  }]
 })
 export class EditVisibleComponent implements OnInit {
 
