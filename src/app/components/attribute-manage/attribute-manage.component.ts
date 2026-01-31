da54b48da227ccdbbe4812bf8315d7c2cc9a2228
Raymond Gigliotti <ray@firestitch.com>
2026-01-31 05:44:45 -0500

    Update package versions and refactor attribute-manage component for improved button usage

--- src/app/components/attribute-manage/attribute-manage.component.ts
+++ src/app/components/attribute-manage/attribute-manage.component.ts
@@ -1,30 +1,29 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
-
-import { AttributeConfig, FsAttributeConfig } from '../../interfaces';
-import { FsDialogModule } from '@firestitch/dialog';
 import { CdkScrollable } from '@angular/cdk/scrolling';
-import { FsAttributeListComponent } from '../attribute-list/attribute-list.component';
-import { MatButton, MatAnchor } from '@angular/material/button';
+import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+
+import { FsDialogModule } from '@firestitch/dialog';
 import { FsFormModule } from '@firestitch/form';
+
+import { FsAttributeListComponent } from '../attribute-list/attribute-list.component';
 
 
 @Component({
-    templateUrl: './attribute-manage.component.html',
-    styleUrls: ['./attribute-manage.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [
-        FsDialogModule,
-        CdkScrollable,
-        MatDialogContent,
-        FsAttributeListComponent,
-        MatDialogActions,
-        MatButton,
-        FsFormModule,
-        MatAnchor,
-    ],
+  templateUrl: './attribute-manage.component.html',
+  styleUrls: ['./attribute-manage.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    FsDialogModule,
+    CdkScrollable,
+    MatDialogContent,
+    FsAttributeListComponent,
+    MatDialogActions,
+    FsFormModule,
+    MatButton,
+  ],
 })
 export class FsAttributeManageComponent {
   data = inject(MAT_DIALOG_DATA);
